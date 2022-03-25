31e353d32321f7802cbb71e826499162d7378aa6
KurtCoban <cengizhan.senol@outlook.com>
2022-03-25 19:50:29 +0300

    switch_case

---
+++ Switch_case/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Switch_case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int month = DateTime.Now.Month;
+
+            switch (month)
+            {
+                case 1: 
+                  Console.WriteLine("Ocaktasın");
+                  break;
+
+                case 2: 
+                  Console.WriteLine("Şubattasın");  
+                  break;
+ 
+                case 3: 
+                  Console.WriteLine("Marttasın");
+                  break;
+
+                case 4: 
+                  Console.WriteLine("Nisandasın");    
+                  break;
+                
+                default:
+                  Console.WriteLine("Yanlış Aydasın");
+                  break;
+            }
+        }
+    }
+}
