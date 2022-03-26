902ab8c51c1aa89fefbdcc00b1b98d9a5f690ac4
KurtCoban <cengizhan.senol@outlook.com>
2022-03-26 13:15:01 +0300

    dizi

---
+++ diziler/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace diziler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] renkler = new string[5];
+            string[] hayvanlar = {"it", "kedo", "birdman", "maymun"};
+
+            int[] dizi;
+            dizi = new int[5];
+
+            renkler[0] ="mavi";
+            dizi[3] = 10;
+            Console.WriteLine(hayvanlar[2]);
+            Console.WriteLine(renkler[0]);
+            Console.WriteLine(dizi[3]);
+
+            Console.WriteLine("Eleman sayısını gir:");
+            int diziUzunlugu = Convert.ToInt32(Console.ReadLine());
+            int[] sayiDizisi = new int[diziUzunlugu];
+            
+            for (int i = 0; i < diziUzunlugu; i++)
+            {
+                Console.Write("{0}. sayıyı gir:", i+1);
+                sayiDizisi[i] = Convert.ToInt32(Console.ReadLine());
+            }
+             
+
+             int toplam = 0;
+            foreach (var sayi in sayiDizisi)
+            {
+                toplam += sayi;
+            }
+            Console.WriteLine("Ortalama:" + toplam/diziUzunlugu);
+
+
+        }
+    }
+}
