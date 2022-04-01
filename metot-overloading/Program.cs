a61ee367c2e462dac997f0a931bd3e1e5fbefbfc
KurtCoban <cengizhan.senol@outlook.com>
2022-04-01 17:06:23 +0300

    metot_2

---
+++ metot-overloading/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace metot_overloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sayi = "999";
+            
+
+            bool sonuc = int.TryParse(sayi, out int outSayi);
+            if(sonuc)
+            {
+                Console.WriteLine("Başarılı");
+                Console.WriteLine(outSayi);
+            }
+            else
+            {
+                Console.WriteLine("Başarısız");
+            }
+
+            Metotlar instance = new Metotlar();
+            instance.Topla(4, 5, out int toplamSonucu);
+            Console.WriteLine(toplamSonucu);
+        }
+    }
+
+    class Metotlar
+    {
+        public void Topla(int a, int b, out int toplam)
+        {
+          
+
+            toplam = a+b;
+
+        
+        }
+    }
+}
