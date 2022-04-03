2c08a7b76332efaef3a228d13f950ad5d34327b4
KurtCoban <cengizhan.senol@outlook.com>
2022-04-03 12:35:19 +0300

    a

---
+++ dictionary/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> kullanıcılar = new Dictionary<int, string>();
+            
+            kullanıcılar.Add(10, "Cengizhan Şenol");
+            kullanıcılar.Add(18, "Sinan Engin");
+            kullanıcılar.Add(19, "Ahmet Çakar");
+
+            Console.WriteLine("**************Elemanlara Erişim*************");
+
+            Console.WriteLine(kullanıcılar[18]);
+            foreach (var item in kullanıcılar)
+                Console.WriteLine(item);
+            
+            Console.WriteLine(kullanıcılar.Count);
+            Console.WriteLine(kullanıcılar.ContainsKey(18));
+            Console.WriteLine(kullanıcılar.ContainsValue("Sinan Engin"));
+
+            
+        }
+    }
+}
