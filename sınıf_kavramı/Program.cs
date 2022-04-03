a2804077e08a0a228363d3e5481aac31c84827bc
KurtCoban <cengizhan.senol@outlook.com>
2022-04-03 18:08:00 +0300

    Sınıf

---
+++ sınıf_kavramı/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace sınıf_kavramı
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Calısan calısan1 = new Calısan();
+            calısan1.Ad = "cengiz";
+            calısan1.Soyad = "Senol";
+            calısan1.No = 31;
+            calısan1.Departman = "İK";
+            calısan1.CalısanBilgileri();
+        }
+    }
+    class Calısan
+    {
+        public string Ad;
+        public string Soyad;
+        public int No;
+        public string Departman;
+        public void CalısanBilgileri()
+        {
+            Console.WriteLine("Çalışan Adı: {0}", Ad);
+            Console.WriteLine("Çalışan Soyadı: {0}", Soyad);
+            Console.WriteLine("Çalışan Numarası: {0}", No);
+            Console.WriteLine("Çalışan Departmanı: {0}", Departman);
+        }
+
+    }
+}
